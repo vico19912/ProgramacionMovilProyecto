4712529971cdd007ee636ca5532a73fbe7fabfc6
Edwin <edwinnoe242@gmail.com>
2025-06-09 21:57:16 -0600

    Ordene las pantallas en sus carpetas correspondientes y agrege una unterfaz principal para el desarrollo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:programacion_movil_proyecto/screens/add_vehicle.dart';
+import 'package:programacion_movil_proyecto/screens/catalog.dart';
 import 'package:programacion_movil_proyecto/widgets/modal.dart';
+import './screens/edit_vehicle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +23,18 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  
+  Home({super.key});
+  
+  final Map<String, dynamic> vehicle = {
+    'marca': 'Honda',
+    'modelo': 'Civic',
+    'imageUrl': 'https://res.cloudinary.com/dtpkeixv3/image/upload/v1745642799/20250425_224637/zheabyochlduxxy8xyeu.jpg',
+    'millas': '20000',
+    'anio': '2017',
+    'descripcion': 'En buen estado',
+    'precio': '12000',
+  };  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +44,7 @@ class Home extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Botones para ver pantallas
             Center(
               child: ElevatedButton(
                 onPressed: () {
@@ -41,6 +55,29 @@ class Home extends StatelessWidget {
                 child: Text("Agrega Vehículo"),
               ),
             ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => EditVehicle(vehicle: vehicle,)),
+                  );
+                },
+                child: Text("Editar Vehículo"),
+              ),
+            ),
+
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => CatalogScreen()),
+                  );
+                },
+                child: Text("Catalago"),
+              ),
+            ),
+
+
             Row(
               children: [
                 Expanded(
